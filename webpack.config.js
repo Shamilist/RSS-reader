91c53648c6b214f4cd150583417f60d06e1b980c
Shamil <shamilist@mail.ru>
2022-05-28 16:50:55 +0300

    delete dist in custom.css and add entry in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
   module: {
     rules: [
       {

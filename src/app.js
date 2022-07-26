fe872f2a1f54d5fb36438db59fb4c88f951b1e22
Shamil <shamilist@mail.ru>
2022-07-26 12:52:32 +0300

    add elemets in app.js

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import * as yup from 'yup';
+import i18next from 'i18next';
+import onChange from 'on-change';
+import axios from 'axios';
+import _ from 'lodash';
+
+
+export default () => {
+
+const elements = {    
+form: document.querySelector('.rss-form'),
+input: document.getElementById('url-input'),
+button: document.querySelector('[type="submit"]'),
+}
+};
+

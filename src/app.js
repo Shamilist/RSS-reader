d06ab6b9d0cdb52d222eb4bc38ebaa96aceff449
Shamil <shamilist@mail.ru>
2022-08-05 12:55:01 +0300

    add all elements

--- src/app.js
+++ src/app.js
@@ -11,6 +11,13 @@ const elements = {
 form: document.querySelector('.rss-form'),
 input: document.getElementById('url-input'),
 button: document.querySelector('[type="submit"]'),
+feedback: document.getElementById('feedback'),
+modalTitle: document.getElementById('modal-title'),
+modalBody: document.getElementById('modal-body'),
+modalLink: document.getElementById('modal-link'),
+posts: document.getElementById('posts'),
+content: document.getElementById('content'),
+feeds: document.getElementById('feeds'),
 }
 };
 

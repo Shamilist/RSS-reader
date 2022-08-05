b700148e44516b8dcf83edd28567c24a037bd155
Shamil <shamilist@mail.ru>
2022-08-05 16:57:11 +0300

    add ru locales translation

---
+++ locales/ru.js
@@ -0,0 +1,12 @@
+const translation = {
+  required: 'Не должно быть пустым',
+  url: 'Ссылка должна быть валидным URL',
+  notOneOf: 'RSS уже существует',
+  success: 'RSS успешно загружен',
+  show: 'Просмотр',
+  network: 'Ошибка сети',
+  parsing: 'Ресурс не содержит валидный RSS',
+  unknown: 'Неизвестная ошибка',
+};
+
+export default translation;

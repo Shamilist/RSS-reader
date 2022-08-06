72815d4cf25397455b3ebfc66a1569dada5ee5db
Shamil <shamilist@mail.ru>
2022-08-06 08:28:06 +0300

    add state

--- src/app.js
+++ src/app.js
@@ -4,20 +4,24 @@ import onChange from 'on-change';
 import axios from 'axios';
 import _ from 'lodash';
 
-
 export default () => {
+  const elements = {
+    form: document.querySelector('.rss-form'),
+    input: document.getElementById('url-input'),
+    button: document.querySelector('[type="submit"]'),
+    feedback: document.getElementById('feedback'),
+    modalTitle: document.getElementById('modal-title'),
+    modalBody: document.getElementById('modal-body'),
+    modalLink: document.getElementById('modal-link'),
+    posts: document.getElementById('posts'),
+    content: document.getElementById('content'),
+    feeds: document.getElementById('feeds'),
+  };
 
-const elements = {    
-form: document.querySelector('.rss-form'),
-input: document.getElementById('url-input'),
-button: document.querySelector('[type="submit"]'),
-feedback: document.getElementById('feedback'),
-modalTitle: document.getElementById('modal-title'),
-modalBody: document.getElementById('modal-body'),
-modalLink: document.getElementById('modal-link'),
-posts: document.getElementById('posts'),
-content: document.getElementById('content'),
-feeds: document.getElementById('feeds'),
-}
+  const state = {
+    status: '',
+    feeds: [],
+    posts: [],
+    error: '',
+  };
 };
-

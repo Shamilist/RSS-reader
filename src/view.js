803c038a761bb81b576e9a92df5276ea4242cafb
Shamil <shamilist@mail.ru>
2022-08-15 08:29:03 +0300

    fix view.js

--- src/view.js
+++ src/view.js
@@ -2,29 +2,40 @@ import onChange from 'on-change';
 import _ from 'lodash';
 
 const renderForm = (watchedState, elements, i18) => {
-  if (watchedState.status === 'invalid') {
-    elements.input.classList.add('is-invalid');
-    elements.feedback.className = 'text-danger';
-    elements.feedback.textContent = i18.t(watchedState.error);
-    elements.input.focus();
-  } else if (watchedState.status === 'start') {
-    elements.input.readOnly = true;
-    elements.button.disabled = true;
-  } else if (watchedState.status === 'success') {
-    elements.input.value = '';
-    elements.input.classList.remove('is-invalid');
-    elements.input.readOnly = false;
-    elements.button.disabled = false;
-    elements.feedback.className = 'text-success';
-    elements.feedback.textContent = i18.t('success');
-    elements.input.focus();
-  } else if (watchedState.status === 'failure') {
-    elements.input.classList.add('is-invalid');
-    elements.input.readOnly = false;
-    elements.button.disabled = false;
-    elements.feedback.className = 'text-danger';
-    elements.feedback.textContent = i18.t(watchedState.error);
-    elements.input.focus();
+  switch (watchedState.status) {
+    case ('invalid'):
+      elements.input.classList.add('is-invaid');
+      elements.feedback.className = 'text-danger';
+      elements.feedback.textContent = i18.t(watchedState.error);
+      elements.input.focus();
+      break;
+
+    case ('start'):
+      elements.input.readOnly = true;
+      elements.button.disabled = true;
+      break;
+
+    case ('success'):
+      elements.input.value = '';
+      elements.input.classList.remove('is-invalid');
+      elements.input.readOnly = false;
+      elements.button.disabled = false;
+      elements.feedback.className = 'text-success';
+      elements.feedback.textContent = i18.t('success');
+      elements.input.focus();
+      break;
+
+    case ('failure'):
+      elements.input.classList.add('is-invalid');
+      elements.input.readOnly = false;
+      elements.button.disabled = false;
+      elements.feedback.className = 'text-danger';
+      elements.feedback.textContent = i18.t(watchedState.error);
+      elements.input.focus();
+      break;
+
+    default:
+      throw new Error(`${watchedState.status} - undefined status!`);
   }
 };
 

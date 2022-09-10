e2f14b11b9742f0a6c4a36b6316759315f2cbbda
Shamil <shamilist@mail.ru>
2022-09-10 12:43:29 +0300

    fix3 renderposts open

--- src/view.js
+++ src/view.js
@@ -63,12 +63,12 @@ const renderPosts = (watchedState, elements, i18n) => {
     }
     link.textContent = post.title;
     link.href = post.link;
+    link.setAttribute('rel', 'noopener norefferer');
+    link.setAttribute('target', '_blank');
 
     const button = document.createElement('button');
     button.classList.add('btn', 'btn-outline-primary');
     button.setAttribute('data-bs-toggle', 'modal');
-    button.setAttribute('rel', 'noopener norefferer');
-    button.setAttribute('target', '_blank');
     button.setAttribute('data-bs-target', '#modal');
     button.textContent = i18n.t('show');
 
